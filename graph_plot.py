4aba387c98f944c33394bb4fd1390f37059abef4
tialaron <30555530+tialaron@users.noreply.github.com>
2022-09-09 16:46:36 +0300

    Add files via upload

--- graph_plot.py
+++ graph_plot.py
@@ -15,3 +15,19 @@ def graph_pie(bot1,bot2,bot3,bot4):
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     st.pyplot(fig1)
+
+#Эта функция отвечает за отображение поля среды с ботами
+'''Внутрь подается состояние среды в виде env (одна переменная). Внутри есть ссылки на локации ботов agent.loc'''
+def plot_env_state(env, ax=None, remap_key=None):
+    maps = env.world.maps
+    locs = [agent.loc for agent in env.world.agents]
+
+    if remap_key is None:
+        cmap_order = None
+    else:
+        assert isinstance(remap_key, str)
+        cmap_order = np.argsort(
+            [agent.state[remap_key] for agent in env.world.agents]
+        ).tolist()
+
+    plot_map(maps, locs, ax, cmap_order)
